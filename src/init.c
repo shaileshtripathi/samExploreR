95f7fc8482e1e8df00ddabd56eb0b1a44770e628
shaileshtripathi <shailesh.tripathy@gmail.com>
2016-08-22 16:51:36 +0300

    updated version

---
+++ src/init.c
@@ -0,0 +1,11 @@
+#include "R_ext/Rdynload.h"
+#include "regrsub.h"
+
+static const R_CMethodDef cMethods[]  = {
+  {"R_readSummary_wrapper",(DL_FUNC)&"R_readSummary_wrapper",2},
+  {NULL, NULL, 0}
+};
+
+void R_init_flagme(DllInfo *info){
+  R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+}
---
+++ src/regrsub.h
@@ -0,0 +1,3 @@
+#include <R.h>
+#include <Rinternals.h>
+void R_readSummary_wrapper(int * nargs, char ** argv);
