8ddc3e7d11bcc8ad660391736cba2ca006455908
shaileshtripathi <shailesh.tripathy@gmail.com>
2016-11-23 00:49:03 +0000

    version update, bugs corrected

--- R/featureCounts.R
+++ R/featureCounts.R
@@ -1,105 +1,63 @@
-samExplore <- function (files, annot.inbuilt = "mm10", annot.ext = NULL, isGTFAnnotationFile = FALSE, 
-    GTF.featureType = "exon", GTF.attrType = "gene_id", chrAliases = NULL, 
-    useMetaFeatures = TRUE, allowMultiOverlap = FALSE, minOverlap = 1, 
-    largestOverlap = FALSE, readExtension5 = 0, readExtension3 = 0, 
-    read2pos = NULL, countMultiMappingReads = FALSE, fraction = FALSE, 
-    minMQS = 0, splitOnly = FALSE, nonSplitOnly = FALSE, primaryOnly = FALSE, 
-    ignoreDup = FALSE, strandSpecific = 0, juncCounts = FALSE, 
-    genome = NULL, isPairedEnd = FALSE, requireBothEndsMapped = FALSE, 
-    checkFragLength = FALSE, minFragLength = 50, maxFragLength = 600, 
-    countChimericFragments = TRUE, autosort = TRUE, nthreads = 1, 
-    maxMOp = 10, reportReads = FALSE, subsample_d=1, N_boot=1,
-    countboot=c("all","Assigned", "Unassigned_Ambiguity",
-    "Unassigned_MultiMapping",   "Unassigned_NoFeatures",
-    "Unassigned_Unmapped",       "Unassigned_MappingQuality",
-    "Unassigned_FragmentLength", "Unassigned_Chimera",
-    "Unassigned_Secondary",      "Unassigned_Nonjunction",
-    "Unassigned_Duplicate" )){
-
- .printParam(subsample_d, N_boot) 
- resultlist <- list()
- 
-fc <- featureCounts(
-	files=files, annot.inbuilt =annot.inbuilt , annot.ext = annot.ext,
- isGTFAnnotationFile = isGTFAnnotationFile,
-    GTF.featureType = GTF.featureType, GTF.attrType =GTF.attrType, chrAliases =chrAliases,
-    useMetaFeatures = useMetaFeatures, allowMultiOverlap = allowMultiOverlap, minOverlap = minOverlap,
-    largestOverlap = largestOverlap, readExtension5 = readExtension5, readExtension3 = readExtension3,
-    read2pos = read2pos, countMultiMappingReads = countMultiMappingReads, fraction = fraction,
-    minMQS = minMQS, splitOnly = splitOnly, nonSplitOnly = nonSplitOnly, primaryOnly = primaryOnly,
-    ignoreDup = ignoreDup, strandSpecific = strandSpecific, juncCounts = juncCounts,
-    genome = genome, isPairedEnd = isPairedEnd, requireBothEndsMapped = requireBothEndsMapped,
-    checkFragLength = checkFragLength, minFragLength = minFragLength, maxFragLength = maxFragLength,
-    countChimericFragments = TRUE, autosort = TRUE, nthreads = 1,
-    maxMOp = maxMOp, reportReads=reportReads)
+samExplore <- function(..., subsample_d = 1, N_boot = 1, countboot = c("all", "Assigned", 
+    "Unassigned_Ambiguity", "Unassigned_MultiMapping", "Unassigned_NoFeatures", "Unassigned_Unmapped", 
+    "Unassigned_MappingQuality", "Unassigned_FragmentLength", "Unassigned_Chimera", 
+    "Unassigned_Secondary", "Unassigned_Nonjunction", "Unassigned_Duplicate")) {
     
- ts <- c()
-for(i in 1:ncol(fc$counts)){
-  fctmp <- list()
-  fctmp[["counts"]] <- as.matrix(fc$counts[,i])
-  fctmp[["stat"]] <- fc$stat[,c(1,(i+1))]	
- cat("//========================== Sub sampling ========================\\\\ \n")
- cat(paste("||		Sub sample N: ", i,"			||","\n",sep=""))
- cat("\\\\===================================================================//")
- restmp <- .featureCountsBoot(fctmp,
-  subsample_d=subsample_d, N_boot=N_boot, countboot=countboot)	
- 
- resultlist[[i]] <- restmp[[1]]
- ts <- c(ts, restmp[[2]])
- }
-names(resultlist) <- fc$targets
-names(ts) <- fc$targets
-list(bootres=resultlist, target.size=ts, featuremain=fc)
-}
-
-
-.featureCountsBoot <- function(flcount,
-subsample_d=1, N_boot=10, countboot="all")
-{
-    ts <- seq <- NULL
-	totalcount <- as.numeric(flcount$stat[,2])
-	names(totalcount) <- as.vector(flcount$stat[,1])
-	if(countboot[1]=="all"){
-            ts <- round(sum(totalcount)*subsample_d)
-	    seqtmp <- as.vector(flcount$counts)
-	    names(seqtmp) <- rownames(flcount$counts)		
-	    seq <- c(seqtmp, totalcount[-which(names(totalcount)=="Assigned")])	
-        } 
-	else{
-	    countboot <- unique(c("Assigned",countboot))
-            ts <- round(sum(totalcount[countboot])*subsample_d)
-	    seqtmp <- as.vector(flcount$counts)
-            names(seqtmp) <- rownames(flcount$counts)
-	    tmpx <- totalcount[countboot]
-	    tmpx <- tmpx[-which(names(tmpx)=="Assigned")]
-	    seq <- c(seqtmp, tmpx)	
-	}
-	bootmat <- c()
-   	
-    for(i in 1:N_boot){	
-        tmp <- thinCounts(seq, target.size=ts)
-    	bootmat <- cbind(bootmat, tmp[rownames(flcount$counts),]) 
+    .printParam(subsample_d, N_boot)
+    
+    fc <- NULL
+    fc <- featureCounts(...)
+    if (is.null(fc)) {
+        stop("no output returned")
     }
-    list(bootmat, target.size=unique(ts,sum(tmp)))
-
+    ts <- vector("character", length(fc$targets))
+    resultlist <- vector("list", length(fc$targets))
+    for (i in length(fc$targets)) {
+        fctmp <- list()
+        fctmp[["counts"]] <- as.matrix(fc$counts[, i])
+        fctmp[["stat"]] <- fc$stat[, c(1, (i + 1))]
+        message(paste("Sub sampling: ",fc$targets[i],"\n", sep=""))
+        message(paste("Sub sample N: ", i, "\n", sep = ""))
+        restmp <- .featureCountsBoot(fctmp, subsample_d = subsample_d, N_boot = N_boot, 
+            countboot = countboot)
+        
+        resultlist[[i]] <- restmp[[1]]
+        ts[i] <- c(ts, restmp[[2]])
+    }
+    names(resultlist) <- fc$targets
+    names(ts) <- fc$targets
+    list(bootres = resultlist, target.size = ts, featuremain = fc)
 }
-.printParam <- function(depth=1, boot=1){
- cat("\n\n\n		samExplorer \n")
- cat("		designed to simulate reduces sequencing depth \n\n")
- cat("		with using of Rsubread \n\n\n")
-
- cat("//===================== samExplore setting =================\\\\ \n")
- cat("||							     ||\n")
- cat("||							     ||\n")
- cat("\\\\=========================================================//\n\n")
 
 
- cat("//========================== samExplore setting ============\\\\ \n")
- cat("||							     ||\n")
- cat("||							     ||\n")
- cat(paste("||	       Simulated Depth: ", depth*100,"%  ||", "\n",sep=""))
- cat(paste("||	       Number of sub samples: ", boot,"	  ||","\n",sep=""))
- cat("||							    || \n")
- cat("||					      	            || \n")
- cat("\\\\=========================================================//\n\n")
+.featureCountsBoot <- function(flcount, subsample_d = 1, N_boot = 10, countboot = "all") {
+    ts <- seq <- NULL
+    totalcount <- as.numeric(flcount$stat[, 2])
+    names(totalcount) <- as.vector(flcount$stat[, 1])
+    if (countboot[1] == "all") {
+        ts <- round(sum(totalcount) * subsample_d)
+        seqtmp <- as.vector(flcount$counts)
+        names(seqtmp) <- rownames(flcount$counts)
+        seq <- c(seqtmp, totalcount[-which(names(totalcount) == "Assigned")])
+    } else {
+        countboot <- unique(c("Assigned", countboot))
+        ts <- round(sum(totalcount[countboot]) * subsample_d)
+        seqtmp <- as.vector(flcount$counts)
+        names(seqtmp) <- rownames(flcount$counts)
+        tmpx <- totalcount[countboot]
+        tmpx <- tmpx[-which(names(tmpx) == "Assigned")]
+        seq <- c(seqtmp, tmpx)
+    }
+    bootmat <- c()
+    
+    for (i in 1:N_boot) {
+        tmp <- thinCounts(seq, target.size = ts)
+        bootmat <- cbind(bootmat, tmp[rownames(flcount$counts), ])
+    }
+    list(bootmat, target.size = unique(ts, sum(tmp)))
+    
+}
+.printParam <- function(depth = 1, boot = 1) {
+    message(paste("Simulated Depth: ", depth * 100, "%", "\n", sep = ""))
+    message(paste(" Number of sub samples: ", boot, "\n", sep = ""))
 }
--- R/plotdat.R
+++ R/plotdat.R
@@ -1,123 +1,112 @@
-library(ggplot2) 
-#library(grid)
-library(colorspace)
-library("pBrackets")
+
+
 
 .gg_color_hue <- function(n) {
-  hues = seq(15, 375, length=n+1)
-  hcl(h=hues, l=65, c=100)[1:n]
+    hues = seq(15, 375, length = n + 1)
+    hcl(h = hues, l = 65, c = 100)[1:n]
 }
 
-.bracket <- function(x, width, y, height){
-  data.frame(
-      x=(c(0,1,4,5,6,9,10)/10-0.5)*(width) + x,
-      y=c(0,1,1,2,1,1,0)/2*(height) + y
-  )
+.bracket <- function(x, width, y, height) {
+    data.frame(x = (c(0, 1, 4, 5, 6, 9, 10)/10 - 0.5) * (width) + x, y = c(0, 1, 
+        1, 2, 1, 1, 0)/2 * (height) + y)
 }
-plotsamExplorer <- function(dat, save=FALSE, filename=NULL, p.depth=.90, 
-font.size=3.5, anova=TRUE,x.lab=NULL, y.lab=NULL, leg.lab=NULL){
-pgg <- NULL
-addlines <- function(xc, yc, cl, lab=NULL, font.size){
-   annotate("text", label = lab, fontface="bold", x = xc, y = yc,
-   size = font.size,  colour = cl) 
-}
-ytmp <- max(dat$Value)
-xtmp <- min(dat$Variable)
- 
- L <- length(dat$Value)
- grp <- names(table(dat$Label))
- dpth <- as.numeric(names(table(dat$Variable)))
-
- temp1 <- which((dpth>=p.depth&dpth<1)==TRUE)
- 
- dpth <- dpth[temp1]
- nmfactor <- names(table(dat$Label))
- lenfactor <- length(table(dat$Label))
- xm1 <- min(dpth)
- xm1 <- which(xm1==as.numeric(names(table(dat$Variable))))
- xm2 <- length(table(dat$Variable)) -1
- cols = .gg_color_hue(lenfactor)
-  new.df <- data.frame(fa=rep("# DE genes", L), f = as.factor(dat$Variable), 
-  value = dat$Value, group = as.factor(dat$Label))
-  colar <- "coral"
-  colar2 <- "seagreen3"
- #xm1 <- 11.5
- #xm2 <- 14.5
- pgg <- ggplot(new.df, aes(x = f, y=value, fill=group, color=group)) + 
- geom_boxplot(notch=TRUE, notchwidth=3, 
- position=position_dodge(width=0.0), outlier.size=1) +
- scale_fill_manual(name = "Annotation", values = cols) +
- scale_color_manual(guide="none", values = rep( "gray40", lenfactor)) +
- ylab("") +
- facet_grid(~fa) + 
- theme(legend.position=c(0.1,0.85)) +
- scale_fill_discrete(name="Annotation")
-if(!is.null(leg.lab)){
-pgg <- pgg + scale_fill_discrete(name="Annotation",labels=leg.lab) 
-} 
-if(!is.null(x.lab)){
-pgg <- pgg+ xlab(x.lab)
-}
-if(!is.null(y.lab)){
-pgg <- pgg+ ylab(y.lab)
-}
-#scale_x_discrete(name="xxx")
-#scale_y_discrete(name="yyy")
- 
-# print(xm1)
-if(anova){
- pv <- c()
- crd <- c()
- for(i in 1:length(grp)){
-  p <- summary(exploreRob(dat, grp[i],dpth))
-  pv <- c(pv, round(p[[1]][["Pr(>F)"]][1],digits=7))
-  a <- which(dat$Label==grp[i])
-  xtmp <- max(dat$Variable[a]) 
-  ytmp <- max(dat$Value[a]) 
-  crd <- rbind(crd, c(xtmp,ytmp))
- }
-
- cnt=0
-for(i in 1:lenfactor){
-pgg <- pgg + addlines(length(table(dat$Variable))-3+crd[i,1],crd[i,2]+5,
-cols[i], lab=paste("p-value", pv[i]),font.size) 
-cnt= cnt -50
-}
- #print(dpth)
- for(i in 1:lenfactor){
-    tt <- which(dat$Label==nmfactor[i])
-    #print(tt)
-    #for(j in 1:length(dpth)){
-      tt1 <- which(dat[tt,]$Variable>=dpth[1])
-     ymn <- min(dat[tt,]$Value[tt1])
-     #print(ymn)
-     ymx <- max(dat[tt,]$Value[tt1])
-    #}
-
-  pgg <- pgg + annotate("rect", xmin = xm1-.5, xmax = xm2+.5, 
-  ymin = ymn, ymax = ymx, alpha = .2) 
- }
-}
-
- # annotate("text", label = "r = 3", fontface="bold", x = 13.0, y = 90,  
- #face="bold", size = font.size, colour = "black") 
-
- plot(pgg)
- #grid.brackets(425, 190, 340, 190, lwd=1, h=0.02)
-
-
-if(save){
-  fn <- NULL
-  if(!is.null(filename)){
-    fn <- paste(getwd(),"/",filename, ".pdf", sep="")
-  }
-  else{
-    fn <- paste("plot", runif(1,min=1000,max=5000),".pdf",sep="")
-    fn <- paste(getwd(),"/",fn,sep="")
-  }
-  print(fn)
-  ggsave(filename=fn, plot=pgg)
-  cat(paste("\n file is saved as", fn, "\n \n")) 
-}
+plotsamExplorer <- function(dat, save = FALSE, filename = NULL, p.depth = 0.9, font.size = 3.5, 
+    anova = TRUE, x.lab = NULL, y.lab = NULL, leg.lab = NULL) {
+    pgg <- NULL
+    addlines <- function(xc, yc, cl, lab = NULL, font.size) {
+        annotate("text", label = lab, fontface = "bold", x = xc, y = yc, size = font.size, 
+            colour = cl)
+    }
+    ytmp <- max(dat$Value)
+    xtmp <- min(dat$Variable)
+    
+    L <- length(dat$Value)
+    grp <- names(table(dat$Label))
+    dpth <- as.numeric(names(table(dat$Variable)))
+    
+    temp1 <- which((dpth >= p.depth & dpth < 1) == TRUE)
+    
+    dpth <- dpth[temp1]
+    nmfactor <- names(table(dat$Label))
+    lenfactor <- length(table(dat$Label))
+    xm1 <- min(dpth)
+    xm1 <- which(xm1 == as.numeric(names(table(dat$Variable))))
+    xm2 <- length(table(dat$Variable)) - 1
+    cols = .gg_color_hue(lenfactor)
+    new.df <- data.frame(fa = rep("# DE genes", L), f = as.factor(dat$Variable), 
+        value = dat$Value, group = as.factor(dat$Label))
+    colar <- "coral"
+    colar2 <- "seagreen3"
+    # xm1 <- 11.5 xm2 <- 14.5
+    pgg <- ggplot(new.df, aes(x = f, y = value, color = group)) + geom_boxplot(notch = TRUE, 
+        notchwidth = 3, position = position_dodge(width = 0), outlier.size = 1) + 
+        scale_fill_manual(name = "Annotation", values = cols) + scale_color_manual(guide = "none", 
+        values = rep("gray40", lenfactor)) + ylab("") + facet_grid(~fa) + theme(legend.position = c(0.1, 
+        0.85)) + scale_fill_discrete(name = "Annotation")
+    if (!is.null(leg.lab)) {
+        pgg <- pgg + scale_fill_discrete(name = "Annotation", labels = leg.lab)
+    }
+    if (!is.null(x.lab)) {
+        pgg <- pgg + xlab(x.lab)
+    }
+    if (!is.null(y.lab)) {
+        pgg <- pgg + ylab(y.lab)
+    }
+    # scale_x_discrete(name='xxx') scale_y_discrete(name='yyy')
+    
+    # print(xm1)
+    if (anova) {
+        pv <- c()
+        crd <- c()
+        for (i in 1:length(grp)) {
+            p <- summary(exploreRob(dat, grp[i], dpth))
+            pv <- c(pv, round(p[[1]][["Pr(>F)"]][1], digits = 7))
+            a <- which(dat$Label == grp[i])
+            xtmp <- max(dat$Variable[a])
+            ytmp <- max(dat$Value[a])
+            crd <- rbind(crd, c(xtmp, ytmp))
+        }
+        
+        cnt = 0
+        for (i in 1:lenfactor) {
+            pgg <- pgg + addlines(length(table(dat$Variable)) - 3 + crd[i, 1], crd[i, 
+                2] + 5, cols[i], lab = paste("p-value", pv[i]), font.size)
+            cnt = cnt - 50
+        }
+        # print(dpth)
+        for (i in 1:lenfactor) {
+            tt <- which(dat$Label == nmfactor[i])
+            # print(tt) for(j in 1:length(dpth)){
+            tt1 <- which(dat[tt, ]$Variable >= dpth[1])
+            ymn <- min(dat[tt, ]$Value[tt1])
+            # print(ymn)
+            ymx <- max(dat[tt, ]$Value[tt1])
+            # }
+            
+            pgg <- pgg + annotate("rect", xmin = xm1 - 0.5, xmax = xm2 + 0.5, ymin = ymn, 
+                ymax = ymx, alpha = 0.2)
+        }
+    }
+    
+    # annotate('text', label = 'r = 3', fontface='bold', x = 13.0, y = 90,
+    # face='bold', size = font.size, colour = 'black')
+    
+    plot(pgg)
+    # grid.brackets(425, 190, 340, 190, lwd=1, h=0.02)
+    
+    
+    if (save) {
+        fn <- NULL
+        if (!is.null(filename)) {
+            fn <- paste(getwd(), "/", filename, ".pdf", sep = "")
+        } else {
+            fn <- paste("plot", runif(1, min = 1000, max = 5000), ".pdf", sep = "")
+            fn <- paste(getwd(), "/", fn, sep = "")
+        }
+        print(fn)
+        ggsave(filename = fn, plot = pgg)
+        cat(paste("\n file is saved as", fn, "\n \n"))
+    }
+    invisible(pgg)
 }
 
